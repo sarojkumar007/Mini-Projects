f5063a699dc93637777f5061e0f1490e962aecec
Saroj Kumar Sahoo <41164464+sarojkumar007@users.noreply.github.com>
2022-06-18 19:46:15 +0530

    update links

--- static/main.js
+++ static/main.js
@@ -1,99 +1,99 @@
 const Project_Data = [
-{
- name: 'GitHub Finder',
- tags: ['API','AJAX'],
- link: `https://sarojk18.github.io/Mini-Projects/GithubFinder/`
-},
-{
- name: 'Glass Effect using CSS',
- tags: ['HTML','CSS'],
- link: `https://sarojk18.github.io/Mini-Projects/GlassEffect/`
-},
-{
- name: 'Magic Nav',
- tags: ['CSS','JS'],
- link: `https://sarojk18.github.io/Mini-Projects/MagicNav/`
-},
-{
- name: 'Simple Chat',
- tags: ['JS','WebRTC'],
- link: `https://sarojk18.github.io/Mini-Projects/chat/`
-},
-{
- name: 'Simple Video',
- tags: ['JS','WebRTC'],
- link: `https://sarojk18.github.io/Mini-Projects/videoCall/`
-},
-{
- name: 'Dino Game',
- tags: ['JS'],
- link: `https://sarojk18.github.io/Mini-Projects/DinoGame/`
-},
-{
- name: 'Tic Tac Toe',
- tags: ['JS','CSS'],
- link: `https://sarojk18.github.io/Mini-Projects/TicTacToe/`
-},
-{
- name: 'Tic Tac Toe PureCSS',
- tags: ['CSS'],
- link: `https://sarojk18.github.io/Mini-Projects/TicTacToe_PureCSS/`
-},
-{
- name: 'Encyclopedia Search',
- tags: ['CSS','JS','AJAX'],
- link: `https://sarojk18.github.io/Mini-Projects/EncyclopediaSearch/`
-},
-{
- name: 'Calculator',
- tags: ['CSS','JS'],
- link: `https://sarojk18.github.io/Mini-Projects/calculator/`
-},
-{
- name: 'Stopwatch',
- tags: ['JS','CSS'],
- link: `https://sarojk18.github.io/Mini-Projects/stopwatch/`
-},
-{
- name: 'Memory Game',
- tags: ['HTML','CSS','JS'],
- link: `https://sarojk18.github.io/Mini-Projects/MemoryGame/`
-},
-{
- name: 'Quiz App',
- tags: ['AJAX','CSS','JS'],
- link: `https://sarojk18.github.io/Mini-Projects/QuizApp/`
-},
-{
- name: 'Random Color',
- tags: ['CSS','JS'],
- link: `https://sarojk18.github.io/Mini-Projects/RandomColor/`
-},
-{
- name: 'Restaurant Menu',
- tags: ['JS','CSS'],
- link: `https://sarojk18.github.io/Mini-Projects/RestaurantMenu/`
-},
-{
- name: 'Rock Paper Scissors',
- tags: ['HTML','CSS','JS'],
- link: `https://sarojk18.github.io/Mini-Projects/RockPaperScissors/`
-},
-{
- name: 'Simple Chat Bot',
- tags: ['CSS','JS'],
- link: `https://sarojk18.github.io/Mini-Projects/SimpleChatBot/`
-},
-{
- name: 'Word Game',
- tags: ['HTML','CSS','JS'],
- link: `https://sarojk18.github.io/Mini-Projects/WordGame/`
-}
+  {
+    name: 'GitHub Finder',
+    tags: ['API', 'AJAX'],
+    link: `https://sarojkumar.dev/Mini-Projects/GithubFinder/`
+  },
+  {
+    name: 'Glass Effect using CSS',
+    tags: ['HTML', 'CSS'],
+    link: `https://sarojkumar.dev/Mini-Projects/GlassEffect/`
+  },
+  {
+    name: 'Magic Nav',
+    tags: ['CSS', 'JS'],
+    link: `https://sarojkumar.dev/Mini-Projects/MagicNav/`
+  },
+  {
+    name: 'Simple Chat',
+    tags: ['JS', 'WebRTC'],
+    link: `https://sarojkumar.dev/Mini-Projects/chat/`
+  },
+  {
+    name: 'Simple Video',
+    tags: ['JS', 'WebRTC'],
+    link: `https://sarojkumar.dev/Mini-Projects/videoCall/`
+  },
+  {
+    name: 'Dino Game',
+    tags: ['JS'],
+    link: `https://sarojkumar.dev/Mini-Projects/DinoGame/`
+  },
+  {
+    name: 'Tic Tac Toe',
+    tags: ['JS', 'CSS'],
+    link: `https://sarojkumar.dev/Mini-Projects/TicTacToe/`
+  },
+  {
+    name: 'Tic Tac Toe PureCSS',
+    tags: ['CSS'],
+    link: `https://sarojkumar.dev/Mini-Projects/TicTacToe_PureCSS/`
+  },
+  {
+    name: 'Encyclopedia Search',
+    tags: ['CSS', 'JS', 'AJAX'],
+    link: `https://sarojkumar.dev/Mini-Projects/EncyclopediaSearch/`
+  },
+  {
+    name: 'Calculator',
+    tags: ['CSS', 'JS'],
+    link: `https://sarojkumar.dev/Mini-Projects/calculator/`
+  },
+  {
+    name: 'Stopwatch',
+    tags: ['JS', 'CSS'],
+    link: `https://sarojkumar.dev/Mini-Projects/stopwatch/`
+  },
+  {
+    name: 'Memory Game',
+    tags: ['HTML', 'CSS', 'JS'],
+    link: `https://sarojkumar.dev/Mini-Projects/MemoryGame/`
+  },
+  {
+    name: 'Quiz App',
+    tags: ['AJAX', 'CSS', 'JS'],
+    link: `https://sarojkumar.dev/Mini-Projects/QuizApp/`
+  },
+  {
+    name: 'Random Color',
+    tags: ['CSS', 'JS'],
+    link: `https://sarojkumar.dev/Mini-Projects/RandomColor/`
+  },
+  {
+    name: 'Restaurant Menu',
+    tags: ['JS', 'CSS'],
+    link: `https://sarojkumar.dev/Mini-Projects/RestaurantMenu/`
+  },
+  {
+    name: 'Rock Paper Scissors',
+    tags: ['HTML', 'CSS', 'JS'],
+    link: `https://sarojkumar.dev/Mini-Projects/RockPaperScissors/`
+  },
+  {
+    name: 'Simple Chat Bot',
+    tags: ['CSS', 'JS'],
+    link: `https://sarojkumar.dev/Mini-Projects/SimpleChatBot/`
+  },
+  {
+    name: 'Word Game',
+    tags: ['HTML', 'CSS', 'JS'],
+    link: `https://sarojkumar.dev/Mini-Projects/WordGame/`
+  }
 ]
 
 const getTagHTML = tags => {
   let h = ``
-  for(let t of tags){
+  for (let t of tags) {
     h += `<span class="tag">${t}</span>`
   }
   return h
@@ -101,7 +101,7 @@ const getTagHTML = tags => {
 
 const updateTable = data => {
   let h = ``
-  for(let p of data){
+  for (let p of data) {
     h += `<tr>
             <td>${p.name}</td>
             <td class="df fww">
@@ -132,13 +132,13 @@ updateTable(Project_Data);
 const handleSearch = e => {
   e.preventDefault();
   const query = search.value;
-  if(query){
+  if (query) {
     const searchResult = searchProjects(query)
     updateTable(searchResult)
-  }else{
+  } else {
     updateTable(Project_Data)
   }
 }
 
 search_form.addEventListener('submit', handleSearch)
-search.addEventListener('input',handleSearch)
+search.addEventListener('input', handleSearch)
