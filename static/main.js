bd4ca2efb252b3dba8785fc103c8ebcc8760b8af
Saroj Kumar Sahoo <dev.sarojkumar18@gmail.com>
2024-02-03 02:30:08 +0530

    update: listings

--- static/main.js
+++ static/main.js
@@ -1,113 +1,118 @@
 const Project_Data = [
   {
-    name: 'GitHub Finder',
-    tags: ['API', 'AJAX'],
+    name: "GitHub Finder",
+    tags: ["API", "AJAX"],
     link: `GithubFinder/`,
   },
   {
-    name: 'Glass Effect using CSS',
-    tags: ['HTML', 'CSS'],
+    name: "Glass Effect using CSS",
+    tags: ["HTML", "CSS"],
     link: `GlassEffect/`,
   },
   {
-    name: 'Magic Nav',
-    tags: ['CSS', 'JS'],
+    name: "Magic Nav",
+    tags: ["CSS", "JS"],
     link: `MagicNav/`,
   },
   {
-    name: 'Simple Chat',
-    tags: ['JS', 'WebRTC'],
+    name: "Simple Chat",
+    tags: ["JS", "WebRTC"],
     link: `chat/`,
   },
   {
-    name: 'Simple Video',
-    tags: ['JS', 'WebRTC'],
+    name: "Simple Video",
+    tags: ["JS", "WebRTC"],
     link: `videoCall/`,
   },
   {
-    name: 'Dino Game',
-    tags: ['JS'],
+    name: "Dino Game",
+    tags: ["JS"],
     link: `DinoGame/`,
   },
   {
-    name: 'Tic Tac Toe',
-    tags: ['JS', 'CSS'],
+    name: "Tic Tac Toe",
+    tags: ["JS", "CSS"],
     link: `TicTacToe/`,
   },
   {
-    name: 'Tic Tac Toe PureCSS',
-    tags: ['CSS'],
+    name: "Tic Tac Toe PureCSS",
+    tags: ["CSS"],
     link: `TicTacToe_PureCSS/`,
   },
   {
-    name: 'Encyclopedia Search',
-    tags: ['CSS', 'JS', 'AJAX'],
+    name: "Encyclopedia Search",
+    tags: ["CSS", "JS", "AJAX"],
     link: `EncyclopediaSearch/`,
   },
   {
-    name: 'Calculator',
-    tags: ['CSS', 'JS'],
+    name: "Calculator",
+    tags: ["CSS", "JS"],
     link: `calculator/`,
   },
   {
-    name: 'Stopwatch',
-    tags: ['JS', 'CSS'],
+    name: "Stopwatch",
+    tags: ["JS", "CSS"],
     link: `stopwatch/`,
   },
   {
-    name: 'Memory Game',
-    tags: ['HTML', 'CSS', 'JS'],
+    name: "Memory Game",
+    tags: ["HTML", "CSS", "JS"],
     link: `MemoryGame/`,
   },
   {
-    name: 'Quiz App',
-    tags: ['AJAX', 'CSS', 'JS'],
+    name: "Quiz App",
+    tags: ["AJAX", "CSS", "JS"],
     link: `QuizApp/`,
   },
   {
-    name: 'Random Color',
-    tags: ['CSS', 'JS'],
+    name: "Random Color",
+    tags: ["CSS", "JS"],
     link: `RandomColor/`,
   },
   {
-    name: 'Restaurant Menu',
-    tags: ['JS', 'CSS'],
+    name: "Restaurant Menu",
+    tags: ["JS", "CSS"],
     link: `RestaurantMenu/`,
   },
   {
-    name: 'Rock Paper Scissors',
-    tags: ['HTML', 'CSS', 'JS'],
+    name: "Rock Paper Scissors",
+    tags: ["HTML", "CSS", "JS"],
     link: `RockPaperScissors/`,
   },
   {
-    name: 'Simple Chat Bot',
-    tags: ['CSS', 'JS'],
+    name: "Simple Chat Bot",
+    tags: ["CSS", "JS"],
     link: `SimpleChatBot/`,
   },
   {
-    name: 'Word Game',
-    tags: ['HTML', 'CSS', 'JS'],
+    name: "Word Game",
+    tags: ["HTML", "CSS", "JS"],
     link: `WordGame/`,
   },
   {
-    name: 'Web Note',
-    tags: ['HTML', 'CSS', 'JS'],
+    name: "Web Note",
+    tags: ["HTML", "CSS", "JS"],
     link: `WebNote/`,
   },
   {
-    name: 'Piano',
-    tags: ['HTML', 'CSS', 'JS'],
+    name: "Piano",
+    tags: ["HTML", "CSS", "JS"],
     link: `Piano/`,
   },
   {
-    name: 'Local Image Editor',
-    tags: ['HTML', 'CSS', 'JS'],
+    name: "Local Image Editor",
+    tags: ["HTML", "CSS", "JS"],
     link: `ImageEditor/`,
   },
   {
-    name: 'Timer Count Down',
-    tags: ['HTML', 'CSS', 'JS'],
+    name: "Timer Count Down",
+    tags: ["HTML", "CSS", "JS"],
     link: `Timer/`,
+  },
+  {
+    name: "EMI Calculator",
+    tags: ["HTML", "CSS", "JS"],
+    link: `EMICalculator/`,
   },
 ];
 
@@ -143,7 +148,7 @@ const updateTable = (data) => {
 
 const searchProjects = (query) => {
   query = query
-    .split(' ')
+    .split(" ")
     .filter((q) => q)
     .map((q) => q.toLowerCase());
   return Project_Data.filter(
@@ -167,5 +172,5 @@ const handleSearch = (e) => {
   }
 };
 
-search_form.addEventListener('submit', handleSearch);
-search.addEventListener('input', handleSearch);
+search_form.addEventListener("submit", handleSearch);
+search.addEventListener("input", handleSearch);
